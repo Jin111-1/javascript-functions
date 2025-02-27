008e29ce05d92290a03194a87791d76a2243a215
Tor <p.sripayom@gmail.com>
2025-02-27 13:56:45 +0700

    JavaScript Functions Assignment

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,27 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+    add: (a,b) => a+b,
+    subtract: (a,b) => {
+        return a-b
+    }
+};
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+
+
+calculator["multiply"] = multiply = (a, b) => a*b;
+calculator.divide = divide;
+
+let addResult = calculator.add(10,20);
+let divideResult = calculator.divide(3000,10);
+
+console.log(calculator)
+console.log(addResult)
+console.log(divideResult)
